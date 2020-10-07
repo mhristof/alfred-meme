33b508f4a81dfd8a997ab4bd98f447d0fffa71f8
Mike Christof <mhristof@gmail.com>
2020-10-07 17:49:10 +0100

    added wrapper script

---
+++ alfred-meme.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+
+GOBIN=$(basename $0 .sh)
+xattr "$GOBIN" | grep 'com.apple.quarantine' && {
+    osascript -e 'display dialog "Alfred: Do you want to trust this version of paste ?" buttons {"No", "Yes"}' | grep 'returned:Yes' && xattr -d com.apple.quarantine "$GOBIN"
+}
+
+./$GOBIN $*
+
+exit 0
+
+
+exit 0
